b5d180274eb81d8886f00d29c5c388071810bc6b
Andres Farias <37941318+anfarias@users.noreply.github.com>
2018-04-03 23:46:27 -0300

    Modificion TP0

---
+++ 00-CppHelloWorld/hello.cpp
@@ -0,0 +1,23 @@
+// First Hello World for AED
+/*K1051
+Farias Andres
+142-155-4
+2018
+Ing. Sola
+*/
+
+//inico de Codigo
+
+#include "stdafx.h"
+#include <iostream>
+
+int main()
+{
+
+	std::cout << "Hello World!\n";
+
+	system("pause");
+
+    return 0;
+}
+
