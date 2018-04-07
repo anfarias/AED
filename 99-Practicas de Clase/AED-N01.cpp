c5a40fdf81a3ef22febe3d11a8b9280038b898ff
Andres Farias <37941318+anfarias@users.noreply.github.com>
2018-04-07 10:31:39 -0300

    Practica Clase 2018-ABR-05
    Practica de la clase de sumas y variables

---
+++ 99-Practicas de Clase/AED-N01.cpp
@@ -0,0 +1,42 @@
+/*Practica de lo visto en la ultima clase 
+2018-04-05 (YYYY-MM-DD)
+K1051
+Jose Maria Sola
+142-155-4
+*/
+
+#include<iostream>
+
+#pragma region Code_main
+
+int main()
+{
+	
+	//definicion de variables
+	double a;
+	double b;
+
+	//ingreso de datos
+
+	std::cout << "Por favor ingrese el Primer numero, \n";
+	std::cin >> a;
+
+	std::cout << "Por favor ingrese el segundo numero,\n";
+	std::cin >> b;
+
+
+	//muestra de resultado
+	std::cout << "Su resultado es, "<< a + b;
+	std::cout << "\n";
+
+
+	//finalizacion y vuelta a inicio
+	system("pause");
+	main();
+}
+
+#pragma endregion
+
+
+
+
