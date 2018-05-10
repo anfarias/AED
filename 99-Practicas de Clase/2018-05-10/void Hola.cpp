8022f6bae71c5e13bbedde2e1cf126282250277c
Andres Farias <37941318+anfarias@users.noreply.github.com>
2018-05-10 20:45:11 -0300

    trash

---
+++ 99-Practicas de Clase/2018-05-10/void Hola.cpp
@@ -0,0 +1,17 @@
+/*practica de void
+farias andres
+2018-05-10 YYYY-MM-DD
+*/
+
+#include <iostream>
+#include <cassert>
+#include <string>
+
+using std:string
+using std:cout
+using std:cin
+
+int main (){
+	
+	void Saludarmundo ();
+	
