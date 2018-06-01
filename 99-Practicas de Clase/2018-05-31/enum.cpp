aa80068caab6e8d33df9a693c7f88daa36ec668b
Andres Farias <37941318+anfarias@users.noreply.github.com>
2018-06-01 00:46:29 -0300

    subida de archivos
    carga de archivos de practica.

---
+++ 99-Practicas de Clase/2018-05-31/enum.cpp
@@ -0,0 +1,35 @@
+/*Practica de Clase
+Enumeracion 
+2018-05-31 (YYYY-MM-DD)
+K1051
+Farias Andres Sebastian
+142-155-4
+*/
+
+//inicio
+
+
+	enum struct Dia {lunes, martes, miercoles, Jueves, viernes, sabado};
+	enum struct Turno {maniana, tarde, noche};
+	
+
+int main (){
+	
+	
+	
+	
+	
+}
+
+
+
+// Declaraciones
+
+bool TengoQueCursar (Dia d, Turno t){
+	return  d==Dia::lunes   and t==Turno::noche
+		or
+			d==Dia::Jueves  and t==Turno::noche;
+			
+	}
+	
+	
---
+++ 99-Practicas de Clase/2018-05-31/void.cpp
@@ -0,0 +1,6 @@
+mostraradicionde2numeros(solicitarnum(),solicitarnum());
+
+
+//ejemplo RAMIS
+
+saludarconnombreyapellido(solicitarnombre(), solicitarapellido());
