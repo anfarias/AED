611ef88ca72a20e1bd55cecde447f6a32acc6f6f
Andres Farias <37941318+anfarias@users.noreply.github.com>
2018-09-06 19:48:53 -0300

    subida de clases

---
+++ 99-Practicas de Clase/2018-08-23/2018-08-23.cpp
@@ -0,0 +1,70 @@
+/*matrices*/
+
+//leer el wordpress del profesor de matrices
+
+//IPO
+//$*-->(P)-->$
+
+
+
+--------------
+//necesidad #1
+
+#include <iostream>
+int main (){
+
+
+doble imp, total=0;
+while (cin>>imp)
+	total +=imo;
+std::cout<<total;
+
+}
+
+
+---------------------------
+//Necesidad #2
+
+double imp;
+unsigned mes;
+array <double,12> total={0};
+
+while (cin>>imp>>mes)
+	total .at(mes-1)+imp;
+for (int i=0;i<12;++i)
+	std::cout>>i+1>>":">>total.at(i)>>'\n';
+
+
+--------------------------
+
+
+//Necesidad #3
+
+#include <array>
+#include <iostream>
+
+int main(){
+	
+	double imp;
+	char v;
+	unsigned mes;
+	array < array < double,12>,3> total {0};
+	
+	
+	while (cin>>imp>>mes>>v)
+		total.at(v-'A').at(mes-1)+=imp;
+	
+	
+	
+	TERMINAR DESPUES SOLO
+	
+	
+	
+}
+__________________________________________________________________
+
+
+
+
+listas enlazadas
+pilas=stacks
---
+++ 99-Practicas de Clase/2018-08-23/Solucion_1.cpp
@@ -0,0 +1,18 @@
+/*Practica de matrices
+Farias Andres Sebastian
+142-155-4
+AED
+*/
+
+//Solucion #1
+
+#include <iostream>
+
+int main(){
+	
+	double imp, total = 0;
+	
+	while (std::cin>>imp)
+		total +=imp;
+	std::cout<<"la suma total es de"<<total<<'\n';
+}
